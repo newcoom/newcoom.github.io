03d8115c1aba79d70b7eeeb0b4c07dd605a65673
riccardoamici <Riccardo@RiccardoMac.lan>
2016-02-18 17:58:09 +0100

    min

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -1,1 +1,90 @@
-function initialize(){var e={lat:41.88966612,lng:12.440371721},t=new google.maps.Map(document.getElementById("map-canvas"),{zoom:16,center:e});new google.maps.Marker({position:e,map:t,title:"Hello World!"})}function scrollTo(e){if($(e).size()){var t=$(e).offset().top-top_menu_height;$("html,body").animate({scrollTop:t},"slow")}}var top_menu_height=0;jQuery(function(e){e(window).load(function(){e(".external-link").unbind("click")}),e(document).ready(function(){var t=document.createElement("script");t.type="text/javascript",t.src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&callback=initialize",document.body.appendChild(t),top_menu_height=e(".templatemo-top-menu").height(),e("body").scrollspy({target:"#templatemo-nav-bar",offset:top_menu_height+10}),e(".external-link").unbind("click"),e("#btn-back-to-top").click(function(e){e.preventDefault(),scrollTo("#templatemo-top")}),e(".templatemo-top-menu .navbar-nav a").click(function(t){t.preventDefault();var a=e(this).attr("href");return scrollTo(a),1==e(".navbar-toggle").is(":visible")&&e(".navbar-collapse").collapse("toggle"),e(this).blur(),!1}),e(".templatemo-top-menu ").stickUp(),e(".templatemo-gallery-category a").click(function(t){t.preventDefault(),e(this).parent().children("a").removeClass("active"),e(this).addClass("active");var a=e(this).attr("href");e(".gallery").each(function(){1==e(this).is(":visible")&&e(this).hide()}),e(a).fadeIn()}),e("a.colorbox").colorbox({rel:function(){return e(this).data("group")}})})});
+var top_menu_height = 0;
+jQuery(function($) {
+		$(window).load( function() {
+			$('.external-link').unbind('click');	
+		});
+		
+        $(document).ready( function() {
+
+            // load google map
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+            'callback=initialize';
+        document.body.appendChild(script);
+
+        top_menu_height = $('.templatemo-top-menu').height();
+        // scroll spy to auto active the nav item
+        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
+		$('.external-link').unbind('click');
+
+        // scroll to top
+        $('#btn-back-to-top').click(function(e){
+            e.preventDefault();
+            scrollTo('#templatemo-top');
+        });
+
+        // scroll to specific id when click on menu
+        $('.templatemo-top-menu .navbar-nav a').click(function(e){
+            e.preventDefault(); 
+            var linkId = $(this).attr('href');
+            scrollTo(linkId);
+            if($('.navbar-toggle').is(":visible") == true){
+                $('.navbar-collapse').collapse('toggle');
+            }
+            $(this).blur();
+            return false;
+        });
+
+        // to stick navbar on top
+        $('.templatemo-top-menu ').stickUp();
+
+        // gallery category
+        $('.templatemo-gallery-category a').click(function(e){
+            e.preventDefault(); 
+            $(this).parent().children('a').removeClass('active');
+            $(this).addClass('active');
+            var linkClass = $(this).attr('href');
+            $('.gallery').each(function(){
+                if($(this).is(":visible") == true){
+                   $(this).hide();
+                };
+            });
+            $(linkClass).fadeIn();  
+        });
+
+        //gallery light box setup
+        $('a.colorbox').colorbox({
+                                    rel: function(){
+                                        return $(this).data('group');
+
+                                    }
+        });
+    });
+});
+
+
+function initialize() {
+  var myLatLng = {lat: 41.88966612, lng: 12.440371721};
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    zoom: 16,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
+// scroll animation 
+function scrollTo(selectors)
+{
+
+    if(!$(selectors).size()) return;
+    var selector_top = $(selectors).offset().top - top_menu_height;
+    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+
+}
